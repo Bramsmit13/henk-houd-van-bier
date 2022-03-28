ad5a4caa7e819a72b97e0542cccad0c3c91acb30
Bramsmit13 <100585689+Bramsmit13@users.noreply.github.com>
2022-03-28 14:06:18 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -8,9 +8,9 @@ let fruit: game.LedSprite = null
 let varken: game.LedSprite = null
 varken = game.createSprite(2, 4)
 basic.forever(function () {
-    fruit = game.createSprite(0, 0)
+    fruit = game.createSprite(randint(0, 4), 0)
     for (let index = 0; index < 4; index++) {
-        basic.pause(980)
+        basic.pause(400)
         fruit.change(LedSpriteProperty.Y, 1)
     }
     if (fruit.isTouching(varken)) {
